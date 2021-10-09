392536d8375d9773a1f261e7e35d64aba3b7320f
dcuoliveira <dan.cunhaa@gmail.com>
2021-10-09 18:02:40 -0300

    add first essentials

---
+++ github-ssh-key.sh
@@ -0,0 +1,3 @@
+cd ~/.ssh
+ssh-keygen -o -t rsa -C "dan.cunhaa@gmail.com"
+cat ~/.ssh/id_rsa.pub
---
+++ install-python-pkgs.sh
@@ -0,0 +1,12 @@
+pip3 install pytrends
+pip3 install multiprocessing
+pip3 install tqdm
+pip3 install pandas
+pip3 install sklearn
+pip3 install matplotlib
+pip3 install numpy
+pip3 install seaborn
+pip3 install statsmodels
+pip3 install xgboost
+pip3 install lightgbm
+
