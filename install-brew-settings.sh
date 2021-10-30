53277f51488bc4bde15df0d6bd60f058e3bee551
Daniel Cunha Oliveira <danieloliveira@MacBook-Air-de-Daniel.local>
2021-10-29 22:21:37 -0300

    add brew install commands

---
+++ install-brew-settings.sh
@@ -0,0 +1,5 @@
+curl -O https://raw.githubusercontent.com/Homebrew/install/master/install.sh
+bash install.sh
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/danieloliveira/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
