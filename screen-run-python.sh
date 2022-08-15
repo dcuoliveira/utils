1b4cdaf939b400f007d8c2557ee4798d964a2b48
Daniel Cunha Oliveira <dan.cunhaa@gmail.com>
2022-08-15 04:00:09 -0300

    Create screen-run-python.sh

---
+++ screen-run-python.sh
@@ -0,0 +1,11 @@
+# runs your code without breaking the terminal
+screen python3 run.py
+
+# unix command to get out of the screen run
+# ctrl + shift + D
+
+# linux command to get out of the screen run
+# ctrl + D
+
+# reconnect 
+sreen -r
