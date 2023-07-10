0012a1733c7130dc7cb39da7aaaf01c46d305b50
Daniel Cunha Oliveira <dan.cunhaa@gmail.com>
2023-07-10 07:31:08 -0300

    Update github-ssh-key.sh

--- github-ssh-key.sh
+++ github-ssh-key.sh
@@ -1,3 +1,9 @@
 cd ~/.ssh
-ssh-keygen -t rsa -f ~/.ssh/project-name-rsa -C "project-name"
-cat ~/.ssh/project-name-rsa.pub
+ssh-keygen -t rsa -b 4096 -C "dan.cunhaa@gmail.com"
+cat ~/.ssh/id_rsa.pub
+ssh -T git@github.com
+
+# https://github.com/settings/keys + add ssh key
+# cd to local github repo
+# git remote set-url origin git@github.com:username/repo-name.git
+# try pull/push
