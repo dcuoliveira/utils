5ede848491b5628c142637e6a0454d56afd088be
DCO <dan.cunhaa@gmail.com>
2024-02-14 06:04:45 -0300

    Update copy-files-n-dirs.sh

--- copy-files-n-dirs.sh
+++ copy-files-n-dirs.sh
@@ -1,2 +1,4 @@
-scp US_CRSP_NYSE.txt danielco@pizza.ime.usp.br:Downloads/
-scp -r US_CRSP_NYSE danielco@pizza.ime.usp.br:Downloads/
+# from localcomputer to remote computer
+scp -r /Users/danieloliveira/Documents/Daniel/codes/graph-portfolio-opt/src/data/inputs/US_CRSP_NYSE danielco@pizza.ime.usp.br:Downloads/
+
+# from remote computer to local computer
